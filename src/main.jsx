36e20c8588c7f52de0e57ca0cb3c74ebbfc5052a
TamjidBhai <tamtop2000@gmail.com>
2025-05-10 20:57:49 +0600

    added login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import React from "react";
 
 import App from "./App.jsx";
-import store from "./app/store.js";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,91 +1,34 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-
-import PageHeader from "../../components/pageHeader/PageHeader";
-import AuthHeader from "../../components/authHeader/AuthHeader";
-import { getAuthData, setMessageEmpty } from "../../features/auth/authSlice";
-import { loginUser } from "../../features/auth/authApiSlice";
-import useFormFields from "../../hooks/useFormFields";
-import { createToast } from "../../utils/toast";
-import "./styles/auth.css";
+import { Link } from "react-router-dom";
+import "../../styles/auth.css";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { message, error, user } = useSelector(getAuthData);
-
-  const { input, resetForm, handleInputChange } = useFormFields({
-    auth: "",
-    password: "",
-  });
-
-  const handleUserLogin = (e) => {
-    e.preventDefault();
-
-    dispatch(loginUser(input));
-  };
-
-  useEffect(() => {
-    if (message) {
-      createToast(message, "success");
-      dispatch(setMessageEmpty());
-      resetForm();
-      navigate("/login");
-    }
-
-    if (error) {
-      createToast(error);
-      dispatch(setMessageEmpty());
-    }
-
-    if (user) {
-      navigate("/");
-    }
-  }, [message, error, resetForm, dispatch, navigate, user]);
-
   return (
-    <>
-      <PageHeader title="Sign In Here" />
-      <div className="auth-container">
-        <div className="auth-wrapper">
-          <div className="auth-top">
-            <AuthHeader
-              title="Login to Get Started"
-              desc="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-            />
-            <div className="auth-form">
-              <form onSubmit={handleUserLogin}>
-                <input
-                  type="text"
-                  placeholder="Email or Phone number"
-                  value={input.auth}
-                  onChange={handleInputChange}
-                  name="auth"
-                />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={input.password}
-                  onChange={handleInputChange}
-                  name="password"
-                />
-                <button>Log In</button>
-              </form>
-              <Link to="/forgot" className="forgot-password">
-                Forgot Password
-              </Link>
-            </div>
-          </div>
-
-          <div className="bottom">
-            <Link to="/register" className="email">
-              Create New Account
-            </Link>
+    <div className="auth-container">
+      <div className="auth-wrapper">
+        <div className="auth-top">
+          <h2>Login to Get Started</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="auth-form">
+            <form>
+              <input
+                type="text"
+                placeholder="Email or Phone number"
+                name="auth"
+              />
+              <input type="password" placeholder="Password" name="password" />
+              <button>Log In</button>
+            </form>
+            <Link className="forgot-password">Forgot Password</Link>
           </div>
         </div>
+
+        <div className="bottom">
+          <Link className="active-account">Active account</Link>
+        </div>
       </div>
-    </>
+
+      <div className="img-wrapper"></div>
+    </div>
   );
 };
 
